121e1d11820a94e96c0761983ea3703a10b95221
suryun-ni <tengkusurya29@gmail.com>
2024-02-19 09:41:28 +0700

    first commit

---
+++ app/Http/Controllers/BarangController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\barang;
+use App\Http\Requests\StorebarangRequest;
+use App\Http\Requests\UpdatebarangRequest;
+
+class BarangController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorebarangRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(barang $barang)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(barang $barang)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatebarangRequest $request, barang $barang)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(barang $barang)
+    {
+        //
+    }
+}
